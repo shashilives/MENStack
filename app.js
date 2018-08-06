27b7f931630473e08af5fa7e4bc4bb8172a9c473
Shashi Kumar <shashikumarlives@gmail.com>
2018-08-07 02:45:30 +0530

    adding ideas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
-
+const bodyParser = require('body-parser')
 //connect to mongodb
 let uri = "mongodb://localhost/vidjot-dev";
 
@@ -15,9 +15,18 @@ mongoose.connect(uri)
     console.log(err)
   );
 
-//Handlebar template engine
+//Load Idea Model
+require('./models/Idea');
+
+const idea = mongoose.model('ideas');
+
+//Handlebar template engine middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+//body parser middleware - to parse incoming request bodies 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   const title = "Welcome to VidJot";
@@ -25,6 +34,35 @@ app.get("/", (req, res) => {
     title: title
   });
 });
+
+//Add idea form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add')
+})
+
+//Process form
+app.post('/ideas/', (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: 'please add a title' })
+  }
+  if (!req.body.details) {
+    errors.push({ text: 'please add a details' })
+  }
+
+  if (errors.length > 0) {
+    console.log("Errpr", errors);
+
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send("Passed")
+  }
+})
 
 app.get("/about", (req, res) => {
   res.render("about");
