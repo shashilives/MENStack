7848b3733e764328c6c2ef45e6b8d57086542f8f
Shashi Kumar <shashikumarlives@gmail.com>
2018-08-06 23:43:05 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server started on port ", port);
+});
+
+app.get("/", (req, res) => {
+  console.log("Inside GET Request");
+  res.send("<h1>Hi There</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Welcome to about");
+});
