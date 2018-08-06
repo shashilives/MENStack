ed4a7880c2ed313948389af165cfbeb40aa62556
Shashi Kumar <shashikumarlives@gmail.com>
2018-08-07 00:01:28 +0530

    handle bar example

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require("express");
 
 const app = express();
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log("Server started on port ", port);
+//how middle ware works
+app.use(function(req, res, next) {
+  console.log(Date.now());
+  req.name = "Shashi Kumar";
+  next();
 });
 
 app.get("/", (req, res) => {
+  console.log(req.name);
+
   res.send("<h1>Hi There</h1>");
 });
 
 app.get("/about", (req, res) => {
   res.send("Welcome to about");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server started on port ", port);
 });
